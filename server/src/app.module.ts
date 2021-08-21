7f949e990d5b1c88bc43261a44146ee4a9dc6f52
EdonJung <2donny@naver.com>
2021-08-21 15:31:09 +0900

    server init

---
+++ server/src/app.module.ts
@@ -0,0 +1,10 @@
+import { Module } from 'server/node_modules/@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ server/src/main.ts
@@ -0,0 +1,8 @@
+import { NestFactory } from 'server/node_modules/@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
+}
+bootstrap();
